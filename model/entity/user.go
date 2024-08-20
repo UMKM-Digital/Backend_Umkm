e81ac054783ec839471aff6c637f102693660075
Naufal Adli santosa <adlisantosanaufal@gmail.com>
2024-08-20 12:42:46 +0700

    profile

--- model/entity/user.go
+++ model/entity/user.go
@@ -6,6 +6,8 @@ type UserEntity struct {
 	Id        int    `json:"id"`
 	Username  string `json:"username"`
 	Email     string `json:"email"`
+	Picture string `json:"picture"`
+	Role string `json:"role"`
 }
 
 func ToUserEntity(user domain.Users) UserEntity {
@@ -13,5 +15,7 @@ func ToUserEntity(user domain.Users) UserEntity {
 		Id: user.IdUser,
 	    Username: user.Username,
 		Email:   user.Email,
+		Picture: user.Picture,
+		Role: user.Role,
 	}
 }
